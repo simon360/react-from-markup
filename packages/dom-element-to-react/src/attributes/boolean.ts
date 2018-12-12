6b4e6184bac4567ec0fbd4c26cc3f6b9ca239328
Simon Andrews <me@simonandrews.ca>
2018-12-12 21:25:14 +0000

    refactor: format files using prettier

--- packages/dom-element-to-react/src/attributes/boolean.ts
+++ packages/dom-element-to-react/src/attributes/boolean.ts
@@ -1,26 +1,25 @@
 // Taken from react-dom: https://github.com/facebook/react/blob/aeda7b745d9c080150704feb20ea576238a1b9a1/packages/react-dom/src/shared/DOMProperty.js#L272
 
 export default [
-    "allowfullscreen",
-    "async",
-    "autofocus",
-    "autoplay",
-    "controls",
-    "default",
-    "defer",
-    "disabled",
-    "formnovalidate",
-    "hidden",
-    "itemscope",
-    "loop",
-    "nomodule",
-    "novalidate",
-    "open",
-    "playsinline",
-    "readonly",
-    "required",
-    "reversed",
-    "scoped",
-    "seamless"
-  ];
-  
+  "allowfullscreen",
+  "async",
+  "autofocus",
+  "autoplay",
+  "controls",
+  "default",
+  "defer",
+  "disabled",
+  "formnovalidate",
+  "hidden",
+  "itemscope",
+  "loop",
+  "nomodule",
+  "novalidate",
+  "open",
+  "playsinline",
+  "readonly",
+  "required",
+  "reversed",
+  "scoped",
+  "seamless"
+];
